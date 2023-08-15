dc811c75c9abe0ac928d76ba3b869485d51219e4
verozroke <73778760+verozroke@users.noreply.github.com>
2023-08-15 11:49:46 +0600

    no moderators

--- src/views/Speaker/info/lists/ModeratorList.vue
+++ src/views/Speaker/info/lists/ModeratorList.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="list">
-    <SpeakerCard
-      v-for="mod in mods"
-      :key="mod.id"
-      :name="mod.name"
-      :occupation="mod.occupation"
-      :company="mod.company"
-      :text="mod.text"
-    />
+    <!-- <SpeakerCard v-for="mod in mods" :key="mod.id" :name="mod.name" :occupation="mod.occupation" :company="mod.company"
+      :text="mod.text" /> -->
   </div>
 </template>
 
@@ -16,41 +10,7 @@ import { ref } from 'vue'
 import SpeakerCard from './card/SpeakerCard.vue'
 
 const mods = ref([
-  {
-    id: 1,
-    name: 'Вася Пупкин',
-    occupation: 'Обзорщик аниме',
-    company: 'ООО Умных',
-    text: 'Бро, тебе надо тренироваться'
-  },
-  {
-    id: 2,
-    name: 'Вася Пупкин',
-    occupation: 'Обзорщик аниме',
-    company: 'ООО Умных',
-    text: 'Бро, тебе надо тренироваться'
-  },
-  {
-    id: 3,
-    name: 'Вася Пупкин',
-    occupation: 'Обзорщик аниме',
-    company: 'ООО Умных',
-    text: 'Бро, тебе надо тренироваться'
-  },
-  {
-    id: 4,
-    name: 'Вася Пупкин',
-    occupation: 'Обзорщик аниме',
-    company: 'ООО Умных',
-    text: 'Бро, тебе надо тренироваться'
-  },
-  {
-    id: 5,
-    name: 'Вася Пупкин',
-    occupation: 'Обзорщик аниме',
-    company: 'ООО Умных',
-    text: 'Бро, тебе надо тренироваться'
-  }
+
 ])
 </script>
 
