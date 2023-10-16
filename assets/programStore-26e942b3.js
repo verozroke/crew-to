4b40fc8199f3eddcf15189a1dcdef2e556b93bef
verozroke <verozroke@users.noreply.github.com>
2023-10-16 11:16:12 +0000

    deploy: 5694dba0ff70a0d4466c3f50a31d630fb0c96662

--- assets/programStore-26e942b3.js
+++
@@ -1,1 +0,0 @@
-import{z as o,r}from"./index-15d2e1d0.js";const s=o("programStore",()=>{const e=r(!1);return{dialog:e,changeDialog:()=>{e.value=!e.value}}});export{s as u};
---
+++ assets/programStore-b80f05ea.js
@@ -0,0 +1,1 @@
+import{z as o,r}from"./index-58ba3051.js";const s=o("programStore",()=>{const e=r(!1);return{dialog:e,changeDialog:()=>{e.value=!e.value}}});export{s as u};
